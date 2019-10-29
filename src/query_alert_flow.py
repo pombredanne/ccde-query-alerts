9efd0d4e163504f793bac7f236527c36b74c2d95
Mark McDonald <mmcdonald@clearcover.com>
2019-10-29 16:19:08 -0500

    adding dynamic flow

---
+++ src/query_alert_flow.py
@@ -0,0 +1,64 @@
+import yaml
+
+from kawasemi import Kawasemi
+from prefect import Flow, task
+from prefect.client import Secret
+from prefect.schedules import Schedule
+from prefect.schedules.clocks import CronClock
+
+from .custom_tasks.snowflakequery import SnowflakeExecution
+
+SNOWFLAKE_ACCOUNT = 'jh72176.us-east-1'
+SNOWFLAKE_USER = 'PREFECT_READ_ONLY'
+SNOWFLAKE_PW = Secret("SNOWFLAKE-READ-ONLY-USER-PW").get()
+SNOWFLAKE_ROLE = 'ANALYST_BASIC'
+SNOWFLAKE_WH = 'COMPUTE_WH'
+SLACK_WEBHOOK = Secret("QUERY-ALERT-SLACK-WH").get()
+
+with open("query_config.yaml", 'r') as stream:
+    data_loaded = yaml.safe_load(stream)
+reports = data_loaded['queries']
+
+crons = set()
+for r in reports:
+    crons.add(r.get('cron_schedule', ''))
+
+for cron in crons:
+    schedule = Schedule(clocks=[CronClock(cron)])
+    schedule.next(5)
+    flow_name = 'query_executions-' + cron
+
+    @task
+    def slack_query_alert(row_count, report):
+        if row_count > 0:
+            slack_config = {"CHANNELS":
+                                {"slack":
+                                     {"_backend": "kawasemi.backends.slack.SlackChannel",
+                                      "url": SLACK_WEBHOOK,
+                                      "username": "Snowflake Query Alert",
+                                      "channel": r['channel']}
+                                 }
+                            }
+            kawasemi = Kawasemi(slack_config)
+            message = 'Alert for: ' + report['query_name'] + '\n' + \
+                      report['slack_message'] + '\n' + "Current row count is " + str(row_count)
+            kawasemi.send(message)
+
+    with Flow(flow_name, schedule) as flow:
+        for r in reports:
+            if r['cron_schedule'] == cron:
+                query = SnowflakeExecution(
+                    SNOWFLAKE_ACCOUNT,
+                    SNOWFLAKE_USER,
+                    SNOWFLAKE_PW,
+                    database=r.get('database', ''),
+                    schema=r.get('schema', ''),
+                    role=SNOWFLAKE_ROLE,
+                    warehouse=SNOWFLAKE_WH,
+                    query=r.get('query', ''))
+
+                alert = slack_query_alert(query[0], r)
+
+
+
+
