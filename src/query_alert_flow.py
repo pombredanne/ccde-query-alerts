107f1dad46d7a2cb6b61516576df16e5d31cdd95
Mark McDonald <mmcdonald@clearcover.com>
2019-10-30 10:26:46 -0500

    getting rid of custom task class

--- src/query_alert_flow.py
+++ src/query_alert_flow.py
@@ -1,20 +1,11 @@
 import yaml
 
-from kawasemi import Kawasemi
 from prefect import Flow, task
 from prefect.client.secrets import Secret
 from prefect.schedules import Schedule
 from prefect.schedules.clocks import CronClock
+import snowflake.connector as sf
 
-from .custom_tasks.snowflakequery import SnowflakeExecution
-
-SNOWFLAKE_ACCOUNT = 'jh72176.us-east-1'
-SNOWFLAKE_USER = 'PREFECT_READ_ONLY'
-s = Secret("SNOWFLAKE-READ-ONLY-USER-PW")
-SNOWFLAKE_PW = s.get()
-SNOWFLAKE_ROLE = 'ANALYST_BASIC'
-SNOWFLAKE_WH = 'COMPUTE_WH'
-SLACK_WEBHOOK = Secret("QUERY-ALERT-SLACK-WH").get()
 
 with open("query_config.yaml", 'r') as stream:
     data_loaded = yaml.safe_load(stream)
@@ -24,6 +15,34 @@ crons = set()
 for r in reports:
     crons.add(r.get('cron_schedule', ''))
 
+
+@task
+def execute_snowflake_query(schema, database, query):
+    s = Secret("SNOWFLAKE-READ-ONLY-USER-PW")
+    SNOWFLAKE_PW = s.get()
+    connect_params = {
+        "account": 'jh72176.us-east-1',
+        "user": 'PREFECT_READ_ONLY',
+        "password": SNOWFLAKE_PW,
+        "database": database,
+        "schema": schema,
+        "role": 'ANALYST_BASIC',
+        "warehouse": 'COMPUTE_WH',
+    }
+    conn = sf.connect(**connect_params)
+    try:
+        with conn:
+            cursor = conn.cursor()
+            executed = cursor.execute(query)
+            row_count = executed.rowcount
+        conn.close()
+        return row_count, executed
+
+    except Exception as error:
+        conn.close()
+        raise error
+
+
 for cron in crons:
     schedule = Schedule(clocks=[CronClock(cron)])
     schedule.next(5)
@@ -32,14 +51,9 @@ for cron in crons:
     with Flow(flow_name) as flow:
         for r in reports:
             if r['cron_schedule'] == cron:
-                query = SnowflakeExecution(
-                    SNOWFLAKE_ACCOUNT,
-                    SNOWFLAKE_USER,
-                    SNOWFLAKE_PW,
-                    database=r.get('database', ''),
-                    schema=r.get('schema', ''),
-                    role=SNOWFLAKE_ROLE,
-                    warehouse=SNOWFLAKE_WH,
-                    query=r.get('query', ''))
+                db=r.get('database', '')
+                sch=r.get('schema', '')
+                q=r.get('query', '')
+                ex = execute_snowflake_query(sch,db,q)
 
-                row_count = query[0]
+                row_count = ex[0]
