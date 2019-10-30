23e4285e37b2d15b5ef0aa129931ce65e9a373a0
Mark McDonald <mmcdonald@clearcover.com>
2019-10-30 10:54:30 -0500

    adding slack back in

--- src/query_alert_flow.py
+++ src/query_alert_flow.py
@@ -1,19 +1,15 @@
 import yaml
 
+from kawasemi import Kawasemi
 from prefect import Flow, task
 from prefect.client.secrets import Secret
 from prefect.schedules import Schedule
 from prefect.schedules.clocks import CronClock
 import snowflake.connector as sf
 
-
 with open("src/query_config.yaml", 'r') as stream:
     data_loaded = yaml.safe_load(stream)
 reports = data_loaded['queries']
-
-crons = set()
-for r in reports:
-    crons.add(r.get('cron_schedule', ''))
 
 
 @task
@@ -43,17 +39,29 @@ def execute_snowflake_query(schema, database, query):
         raise error
 
 
-for cron in crons:
-    schedule = Schedule(clocks=[CronClock(cron)])
-    schedule.next(5)
-    flow_name = 'query_executions-' + cron
+@task
+def slack_query_alert(row_count, report):
+    SLACK_WEBHOOK = Secret("QUERY-ALERT-SLACK-WH").get()
+    if row_count > 0:
+        slack_config = {"CHANNELS":
+                            {"slack":
+                                 {"_backend": "kawasemi.backends.slack.SlackChannel",
+                                  "url": SLACK_WEBHOOK,
+                                  "username": "Snowflake Query Alert",
+                                  "channel": r['channel']}
+                             }
+                        }
+        kawasemi = Kawasemi(slack_config)
+        message = 'Alert for: ' + report['query_name'] + '\n' + \
+                  report['slack_message'] + '\n' + "Current row count is " + str(row_count)
+        kawasemi.send(message)
 
-    with Flow(flow_name) as flow:
-        for r in reports:
-            if r['cron_schedule'] == cron:
-                db=r.get('database', '')
-                sch=r.get('schema', '')
-                q=r.get('query', '')
-                ex = execute_snowflake_query(sch,db,q)
 
-                row_count = ex[0]
+with Flow('query_alerts') as flow:
+    for r in reports:
+        db = r.get('database', '')
+        sch = r.get('schema', '')
+        q = r.get('query', '')
+        ex = execute_snowflake_query(sch, db, q)
+        row_count = ex[0]
+        slack_query_alert(row_count, r)
