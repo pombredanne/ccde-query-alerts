9817f81c01dee2ab43a2c1b7093cf6f181fdefc2
Mark McDonald <mmcdonald@clearcover.com>
2019-10-30 14:37:33 -0500

    another try

--- src/query_alert_flow.py
+++ src/query_alert_flow.py
@@ -1,91 +1,67 @@
-from datetime import date, datetime, timedelta
 import yaml
 
 from kawasemi import Kawasemi
 from prefect import Flow, task
 from prefect.client import Secret
-from prefect.schedules import Schedule
-from prefect.schedules.clocks import CronClock
 import snowflake.connector as sf
 
-#
-# schedule = Schedule(clocks=[CronClock("0 13 * * *")])
-# schedule.next(5)
-#
-# @task
-# def get_queries():
-#     with open("src/query_config.yaml", 'r') as stream:
-#         data_loaded = yaml.safe_load(stream)
-#     reports = data_loaded['queries']
-#     return reports[0]
-#
-#
-# @task
-# def execute_snowflake_query(di):
-#     s = Secret("SNOWFLAKE-READ-ONLY-USER-PW")
-#     password = s.get()
-#     connect_params = {
-#         "account": 'jh72176.us-east-1',
-#         "user": 'PREFECT_READ_ONLY',
-#         "password": password,
-#         "database": di.get('database',''),
-#         "schema": di.get('schema', ''),
-#         "role": 'ANALYST_BASIC',
-#         "warehouse": 'COMPUTE_WH',
-#     }
-#     conn = sf.connect(**connect_params)
-#     try:
-#         with conn:
-#             cursor = conn.cursor()
-#             executed = cursor.execute(di.get('query', ''))
-#             row_count = executed.rowcount
-#         conn.close()
-#         return row_count, executed
-#
-#     except Exception as error:
-#         conn.close()
-#         raise error
-#
-#
-# @task
-# def slack_query_alert(row_count, report):
-#     webhook = Secret("QUERY-ALERT-SLACK-WH").get()
-#     if row_count > 0:
-#         slack_config = {"CHANNELS":
-#                             {"slack":
-#                                  {"_backend": "kawasemi.backends.slack.SlackChannel",
-#                                   "url": webhook,
-#                                   "username": "Snowflake Query Alert",
-#                                   "channel": report['channel']}
-#                              }
-#                         }
-#         kawasemi = Kawasemi(slack_config)
-#         message = 'Alert for: ' + report['query_name'] + '\n' + \
-#                   report['slack_message'] + '\n' + "Current row count is " + str(row_count)
-#         kawasemi.send(message)
-#
-#
-# with Flow('ETL', schedule) as flow:
-#     queries = get_queries()
-#     ex = execute_snowflake_query(queries)
-#     slack_query_alert(ex[0], queries)
 
 @task
-def extract():
-    return [1, 2, 3]
+def get_queries():
+    with open("src/query_config.yaml", 'r') as stream:
+        data_loaded = yaml.safe_load(stream)
+    reports = data_loaded['queries']
+    return reports
 
 
 @task
-def transform(x):
-    return [i * 10 for i in x]
+def execute_snowflake_query(report):
+    s = Secret("SNOWFLAKE-READ-ONLY-USER-PW")
+    password = s.get()
+    connect_params = {
+        "account": 'jh72176.us-east-1',
+        "user": 'PREFECT_READ_ONLY',
+        "password": password,
+        "database": report.get('database', ''),
+        "schema": report.get('schema', ''),
+        "role": 'ANALYST_BASIC',
+        "warehouse": 'COMPUTE_WH',
+    }
+    conn = sf.connect(**connect_params)
+    try:
+        with conn:
+            cursor = conn.cursor()
+            executed = cursor.execute(report.get('query', ''))
+            row_count = executed.rowcount
+        conn.close()
+        return row_count, report
+
+    except Exception as error:
+        conn.close()
+        raise error
 
 
 @task
-def load(y):
-    print("Received y: {}".format(y))
+def slack_query_alert(query_execution):
+    row_count = query_execution[0]
+    report = query_execution[1]
+    if row_count > 0:
+        webhook = Secret("QUERY-ALERT-SLACK-WH").get()
+        slack_config = {"CHANNELS":
+                            {"slack":
+                                 {"_backend": "kawasemi.backends.slack.SlackChannel",
+                                  "url": webhook,
+                                  "username": "Snowflake Query Alert",
+                                  "channel": report['channel']}
+                             }
+                        }
+        kawasemi = Kawasemi(slack_config)
+        message = 'Alert for: ' + report['query_name'] + '\n' + \
+                  report['slack_message'] + '\n' + "Current row count is " + str(row_count)
+        kawasemi.send(message)
 
 
-with Flow("ETL-test") as flow:
-    e = extract()
-    t = transform(e)
-    l = load(t)
+with Flow('query_alerts') as flow:
+    queries = get_queries()
+    executions = execute_snowflake_query.map(queries)
+    send_alerts = slack_query_alert.map(executions)
